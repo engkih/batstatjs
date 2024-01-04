f7e9a2b568e848d9ef3335891f5c1a6e237dbc96
Achmad Riky <achmadricky01@gmail.com>
2024-01-04 15:13:33 +0700

    add battlevel, charging time, and discharging time

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ window.onload = main;
 
 function main() {
 	const batStat = document.getElementById("cstatus");
+	const batLevel = document.getElementById("blevel");
+	const chargeTime = document.getElementById("ctime");
+	const dischargeTime = document.getElementById("dtime");
 
 	navigator.getBattery().then((batt) => {
 		console.log(batt)
+		console.log(batt.chargingTime);
 	
 		// Initial value setter
-		if (batt.charging) {
-			batStat.textContent = "Charging"
-		}
-		else if (!batt.charging) {
-			batStat.textContent = "Discharging"
-		}
+		updateBatStat();
+		updateChargingTime();
+		updateDischargingTime();
+
+		batLevel.textContent = batt.level*100+"%";
+		
 
 		// Listening to charging status event change then update using updateBatStat()
 		batt.addEventListener("chargingchange", () => {
@@ -22,12 +26,55 @@ function main() {
 
 		function updateBatStat() {
 			if (batt.charging) {
-				batStat.textContent = "Charging"
+				batStat.textContent = "Charging."
+				chargeTime.textContent = "Calculating...."
 			}
 			else if (!batt.charging) {
-				batStat.textContent = "Discharging"
+				batStat.textContent = "Discharging."
+				dischargeTime.textContent = "Calculating...."
 			}
 		}
+
+		// Listening to battery level and update every change.
+		batt.addEventListener("levelchange", () => {
+			batLevel.textContent = batt.level*100+"%";
+		});
+
+
+		// Listening to charging time event change then update using updateChargingTime().
+		batt.addEventListener("chargingtimechange", () => {
+			updateChargingTime()
+		});
+
+		function updateChargingTime() {
+			if (batt.chargingTime == Infinity) {
+				chargeTime.textContent = "Not Charging."
+			}
+			else if (batt.chargingTime != Infinity) {
+				chargeTime.textContent = Math.round(batt.chargingTime / 60) + " minutes.";
+			}
+		};
+		
+		// Listening to discharging time event change then update using updateDischargingTime().
+		batt.addEventListener("dischargingtimechange", () => {
+			updateDischargingTime();
+		});
+
+		function updateDischargingTime() {
+			if (batt.dischargingTime == Infinity) {
+				dischargeTime.textContent = "Battery is charging.";
+			}
+			if (batt.dischargingTime != Infinity) {
+				const dischTime = Math.round(batt.dischargingTime/60);
+				const dischTimeTextM = dischTime + " minutes."
+				if (dischTime > 59) {
+					const dischTimeHour = Math.round(dischTime/60);
+					const dischTimeMinute = dischTime%60;
+				dischargeTime.textContent = dischTimeHour  + " hours, " + dischTimeMinute + " minutes.";
+			}
+		}
+		};
+			
 	});
 
 }
